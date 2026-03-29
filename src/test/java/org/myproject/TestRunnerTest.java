55a67f58b845d6009569ac9dcd8e9e7e1084dae0
souravpattnaik431 <sauravpattnaik674@gmail.com>
2026-03-29 22:47:46 +0530

    remove junit-platform.properties

--- src/test/java/org/myproject/TestRunnerTest.java
+++ src/test/java/org/myproject/TestRunnerTest.java
@@ -6,13 +6,16 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
-import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.*;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectPackages("org.myproject")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "html:target/report.html")
+@ConfigurationParameter(key = EXECUTION_DRY_RUN_PROPERTY_NAME,value = "false")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, summary, " +
+        "html:target/report.html, json:target/cucumber.json, " +
+        "progress:target/cucumber-progress.txt, " +
+        "usage:target/cucumber-usage.json, rerun:target/rerun.txt, junit:target/cucumber-junit.xml")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "org.myproject,types,objects")
 public class TestRunnerTest {
 
